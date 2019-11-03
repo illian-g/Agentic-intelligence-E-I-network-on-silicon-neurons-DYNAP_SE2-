5ec52e05acee3d0d078deb57ad5ae16a01e34400
Nicoletta Risi <nicoletta@ini.uzh.ch>
2019-11-03 18:48:11 +0100

    Add test for controller with rpyc

---
+++ test_CtxctlController_rpyc.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  3 12:41:13 2019
+
+CtxCtl unittest for rpyc connection.
+Run from a parent directory that contains the cortexcontrol folder.
+
+Files required to run the unittest:
+    CtxctlController.py
+    RpycConnector.py
+    run_rpyc.py
+    
+Before running the unitest make sure that all 3 files are in the ctxctl folder.
+
+@author: nrisi
+"""
+
+import unittest
+from cortexcontrol.CtxctlController import CtxctlController
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+SHOW_PLOTS_IN_TESTS = True
+
+class TestCtxctlController(unittest.TestCase):
+    
+    def test___init__(self):
+        print(self.__class__.__name__ + ' : __init__')
+        pass
+    
+    def test_connect(self):
+        """ Test class function self.connect.
+        Connect two cores and check created connections
+        """
+        print(self.__class__.__name__ + ' : test_connect')
+        cc = CtxctlController(backend='rpyc')
+        pre = list(range(1,256))
+        post = list(range(257, 512))
+        
+        # Tested function: Create connections
+        # *********************************************************************        
+        cc.connect(pre, post, syn_type=cc.SynType.FAST_EXC, 
+                   syn_weight=1, 
+                   connection_type='onchip')
+        # *********************************************************************
+        
+        # Check that neuron pre ids are in stored in cams of neuron post
+        cams_id = cc.get_cams(post[0])
+        self.assertIn(pre[0],cams_id)
+
+        # Check attribute receiving connection from:
+        self.assertIn(cc.neurons[pre[0]], 
+                      cc.connector.receiving_connections_from[cc.neurons[post[0]]])
+        
+        if SHOW_PLOTS_IN_TESTS:
+            weight_matrix = cc.get_connectivity_matrix()
+            _ = plt.figure()
+            sns.heatmap(weight_matrix)
+            plt.title('pre-post CAMs')
+            plt.xlabel('post')
+            plt.ylabel('pre')
+            plt.axis('equal')             
+        
+    def test_remove_connection(self):              
+        """ Test class funciton self.remove_connection()
+        """
+        print(self.__class__.__name__ + ' : test_remove_connection')
+        cc = CtxctlController(backend='rpyc')
+        pre = list(range(1025, 1025+256))
+        post = list(range(1025+256, 1025+512))
+
+        # Make sure that all previous connections have been removed correctly
+        cams_id = cc.get_cams(post[0])
+        self.assertNotIn(pre[0],cams_id)
+                
+        # Create connections
+        cc.connect(pre, post, syn_type=cc.SynType.FAST_EXC, 
+                   syn_weight=1, 
+                   connection_type='onchip')
+
+        # Tested function: Remove all connections        
+        # *********************************************************************
+        cc.remove_connection(pre, post)
+        # *********************************************************************
+        
+        # Cehck that neuron pre is no longer listed in neuron post cams
+        cams_id = cc.get_cams(post[0])
+        self.assertNotIn(pre[0],cams_id)
+        
+        
+if __name__ == '__main__':
+    os.chdir('./cortexcontrol/')
+    os.system('./cortexcontrol&')
+    os.chdir('..')
+    print('Start rpyc server by running in the console:')
+    print('>>> import run_rpyc')
+    print('Then press key to start the unittest')
+    _ = input('>>> ')      
+    unittest.main()
