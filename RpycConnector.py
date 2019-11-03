9955f86fa7fe06c76a9692e6c85ad59b9a7dd336
Nicoletta Risi <nicoletta@ini.uzh.ch>
2019-11-03 18:46:22 +0100

    Change class name

--- RpycConnector.py
+++ RpycConnector.py
@@ -5,18 +5,17 @@ class RpycConnector(object):
         """ This connects to rpyc server started in ctxctl python console.
         If connection is already existing, return the current hook
         """
-        if (Ctxctl.__c == None):
+        if (RpycConnector.__c == None):
             
             print('Rpyc : Creating connection to host')
-            Ctxctl.__c = rpyc.classic.connect("localhost", 1300)   
+            RpycConnector.__c = rpyc.classic.connect("localhost", 1300)   
             print('Rpyc : ..done!')
-            print(Ctxctl.__c.modules.CtxDynapse) # prints module address
             
             RPYC_TIMEOUT = 600 #defines a higher timeout
             print('Rpyc : Setting rpyc timeout to', RPYC_TIMEOUT , ' sec')
-            Ctxctl.__c._config["sync_request_timeout"] = RPYC_TIMEOUT  # Set timeout to higher level
+            RpycConnector.__c._config["sync_request_timeout"] = RPYC_TIMEOUT  # Set timeout to higher level
             
-            return Ctxctl.__c
+            return RpycConnector.__c
         else:
             print('Rpyc : Connection already existing')
-            return Ctxctl.__c
+            return RpycConnector.__c
