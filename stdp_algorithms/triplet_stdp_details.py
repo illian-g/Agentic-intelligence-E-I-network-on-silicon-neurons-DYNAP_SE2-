7890a0c8cd6ae7fd8b16f34aa7ef59099fa9b214
Jingyue Zhao <jzhao@ini.uzh.ch>
2022-04-27 17:48:51 +0800

    Finish triplet details doc

--- stdp_algorithms/triplet_stdp_details.py
+++ stdp_algorithms/triplet_stdp_details.py
@@ -3,12 +3,39 @@ import json
 
 class TripletStdp:
     """
-    Implementation of all to all plastic connections between pre and post 
-    population, including trace graph setup and algorithm implementation with
-    retrieved traces. http://arxiv.org/abs/1608.08267.
+    Triplet STDP is implemented. Plastic connections between pre and post 
+    population are updated over time, including trace graph setup and 
+    algorithm implementation with retrieved traces. 
+    See details at http://arxiv.org/abs/1608.08267.
+    
+    Other STDP-like learning algorithms can be
+    implemented by 
+        - defining configuring a filtering graph to get required neuron traces
+            like `set_triplet_stdp_graph` for 1 pre trace and 2 post traces in
+            triplet STDP.
+        - implementing the weight update operations using received traces 
+            as in `triplet_stdp_algorithm`.
 
-    Triplet STDP is implemented, but can be replaced with other Hebbian-like learning algorithms
+    Args:
+        param_file (str): parameter file name that stores algorithm parameters.
 
+    Attributes:
+        method (str): how the neuron trace grows when a neuron fires. 
+            Can be "increase_by" or "increase_to" a constant value. 
+        trace_max (float): maximum trace value since "increase_to" is chosen
+            for triplet-STDP.
+        pre_tau (int): time constant of trace pre in microsecond. The unit is 
+            millisecond in JSON file for simplicity.
+        post1_tau (int): time constant of trace post1 in microsecond. The unit is 
+            millisecond in JSON file for simplicity.
+        post2_tau (int): time constant of trace post2 in microsecond. The unit is 
+            millisecond in JSON file for simplicity.
+        nuEEpre (float): presynaptic spike learning rate.
+        nuEEpost (float): postsynaptic spike learning rate.
+        wmaxEE (float): maximum weight of triplet-STDP.
+        expEEpre (float): presynaptic weight dependence.
+        expEEpost (float): postsynaptic weight dependence.
+    
     Algorithm:
         On pre: neuron select node of pre -> pre timestamps
             update post_trace1: post_trace1 values at pre timestamps
@@ -26,18 +53,6 @@ class TripletStdp:
             w_plast = clip(w_plast + delta_w, 0, wmaxEE)
             post1 = 1.0
             post2 = 1.0
-    
-    Parameters:
-        method = "increase_to", # "increase_by",  "increase_to"
-        trace_max = 1, # maximum trace value
-        pre_tau = 20, # time constant of trace pre, millisec in JSON file
-        post1_tau = 40, # time constant of trace post1, millisec in JSON file
-        post2_tau = 40, # time constant of trace post2, millisec in JSON file
-        nuEEpre = 0.005, # presynaptic spike learning rate
-        nuEEpost = 0.025, # postsynaptic spike learning rate
-        wmaxEE = 1, # maximum weight
-        expEEpre = 0.2, # presynaptic weight dependence
-        expEEpost = 0.2 # postsynaptic weight dependence
     """ 
     def __init__(self,
                  param_file):
@@ -58,9 +73,22 @@ class TripletStdp:
         self.expEEpost = data["expEEpost"]
 
     def triplet_stdp_algorithm(self, w_plast, onpre_traces, onpost_traces, pre_neuron_ids, post_neuron_ids):
-        """
-        The implementation of triplet stdp algorithm given Dynapse1traces retrieved from the trace graph.
-        """
+        """The implementation of triplet stdp algorithm given Dynapse1traces 
+        retrieved from the trace graph. LTD when at pre spikes, LTP at
+        post spikes using the pre, post1, post2 traces.
+
+        Args:
+            w_plast (numpy.array): current plastic weight matrix, changing during learning.
+            onpre_traces (list[samna.dynapse1.Dynapse1Trace]): onpre traces
+                which triggers LTD.
+            onpost_traces (list[samna.dynapse1.Dynapse1Trace]): onpost traces
+                which triggers LTP.
+            pre_neuron_ids (list[tuple(int,int,int)]): presynaptic neuron IDs (chip, core, neuron). 
+            post_neuron_ids (list[tuple(int,int,int)]): postsynaptic neuron IDs (chip, core, neuron).
+
+        Returns:
+            w_plast (numpy.array): plastic weight matrix after this run of weight update.
+        """        
         # on pre, LTD, contains post1 trace
         for onpre_trace in onpre_traces:
             # for each pre spike timestamp
@@ -95,9 +123,23 @@ class TripletStdp:
         return w_plast
         
     def set_triplet_stdp_graph(self, spike_filter_node, onpre_trace_node, onpost_trace_node, pre_neuron_ids, post_neuron_ids):
-        """
-        Set up trace graph to calculate the traces for selected neurons.
-        """
+        """Set up trace graph to calculate the required traces using related neurons.
+        Here, pre and post2 traces are needed for LTP at post spiking times. 
+        Thus, the trigger neurons of these two traces are the post neurons as in 
+        onpost_trace_node. Simliarly, post1 trace is required for LTD process at
+        pre spiking times, and thus triggered by pre neurons in onpre_trace_node.
+
+        Args:
+            spike_filter_node: passes spikes of pre and post neurons.
+            onpre_trace_node: tracks and generates traces that are updated 
+                at pre neurons' spiking times, which triggers LTD.
+            onpost_trace_node: tracks and generates traces that are updated 
+                at post neurons' spiking times, which triggers LTP.
+            pre_neuron_ids (list[tuple(int,int,int)]): presynaptic neuron 
+                IDs (chip, core, neuron). 
+            post_neuron_ids (list[tuple(int,int,int)]): postsynaptic neuron 
+                IDs (chip, core, neuron).
+        """        
         # configure filter nodes: which neurons to filter
         spike_filter_node.set_neurons(pre_neuron_ids+post_neuron_ids)
 
